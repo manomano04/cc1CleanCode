01a5b26d9878b84d9b26056d8025a64d56263af4
manon <manon.rouleau@panorabanques.com>
2025-10-30 10:37:28 +0100

    ajout cas figure chance

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const FIGURES = {
+    YAMS: { name: "Yams", points: 50 },
+    GRANDE_SUITE: { name: "Grande suite", points: 40 },
+    CARRE: { name: "Carré", points: 35 },
+    FULL: { name: "Full", points: 30 },
+    BRELAN: { name: "Brelan", points: 28 },
+};
 function compterOccurrences(des) {
     return des.reduce((occurence, value) => {
         occurence[value] = (occurence[value] || 0) + 1;
@@ -7,10 +14,16 @@ function compterOccurrences(des) {
 
 function analyzeDices(dice) {
     const occurrence = compterOccurrences(dice);
+    const values = Object.values(occurrence);
     const max = Math.max(...Object.values(occurrence));
     if (max === 5) return 50
     if (max === 4) return 35;
+    if (values.includes(3) && values.includes(2)) return 30;
     if (max === 3) return 28;
+
+    const sumDice = dice.reduce((total, dice) => total + dice, 0);
+    return sumDice;
+
 }
 
 export const test = (dices) => {
--- index.test.js
+++ index.test.js
@@ -2,9 +2,24 @@ import { describe, it, expect } from "vitest";
 import {test} from "./index.js";
 describe("index", () => {
 
+    it("test chance", () => {
+        const dices = [[1,1,4,6,3]]
+        expect(test(dices)).toBe(15);
+    })
+
     it("yams : should return 50", () => {
         const dices = [[1,1,1,1,1]]
         expect(test(dices)).toBe(50);
+    })
+
+    it("carré : shoul return 35", () => {
+        const dices = [[1,1,1,1,2]]
+        expect(test(dices)).toBe(35);
+    })
+
+    it("full : should return 30", () => {
+        const dices = [[1,1,1,2,2]]
+        expect(test(dices)).toBe(30);
     })
 
     it("brelan : shoul return 28", () => {
@@ -12,8 +27,12 @@ describe("index", () => {
         expect(test(dices)).toBe(28);
     })
 
-    it("carré : shoul return 35", () => {
-        const dices = [[1,1,1,1,2]]
-        expect(test(dices)).toBe(35);
+    it("test few figures : brelan and carré should return 63 ", () => {
+        const dices = [[1,1,1,1,2], [5,4,2,2,2]]
+        expect(test(dices)).toBe(63);
     })
+
+
+
+
 })
