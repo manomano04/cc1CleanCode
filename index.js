3641d96b63ce3cd74468f2fd8e63ccdef4c0f44a
manon <manon.rouleau@panorabanques.com>
2025-10-30 10:44:05 +0100

    modify dict and update function name

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const FIGURES = {
-    YAMS: { name: "Yams", points: 50 },
-    GRANDE_SUITE: { name: "Grande suite", points: 40 },
-    CARRE: { name: "Carré", points: 35 },
-    FULL: { name: "Full", points: 30 },
-    BRELAN: { name: "Brelan", points: 28 },
+    YAMS: 50,
+    GRANDE_SUITE: 40,
+    CARRE: 35,
+    FULL: 30,
+    BRELAN: 28,
 };
-function compterOccurrences(des) {
+function countOccurrences(des) {
     return des.reduce((occurence, value) => {
         occurence[value] = (occurence[value] || 0) + 1;
         return occurence;
@@ -13,17 +13,16 @@ function compterOccurrences(des) {
 }
 
 function analyzeDices(dice) {
-    const occurrence = compterOccurrences(dice);
+    const occurrence = countOccurrences(dice);
     const values = Object.values(occurrence);
     const max = Math.max(...Object.values(occurrence));
-    if (max === 5) return 50
-    if (max === 4) return 35;
-    if (values.includes(3) && values.includes(2)) return 30;
-    if (max === 3) return 28;
+    if (max === 5) return FIGURES.YAMS
+    if (max === 4) return FIGURES.CARRE;
+    if (values.includes(3) && values.includes(2)) return FIGURES.FULL;
+    if (max === 3) return FIGURES.BRELAN;
 
     const sumDice = dice.reduce((total, dice) => total + dice, 0);
     return sumDice;
-
 }
 
 export const test = (dices) => {
